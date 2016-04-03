54469ba8367d112fe9d814755e3b3d6d6dcf9dec
Kevin <kevinchou95@gmail.com>
2016-04-03 15:05:20 +0200

    Fixed compilation errors

--- chisel/RegStatus.scala
+++ chisel/RegStatus.scala
@@ -14,28 +14,26 @@ class RegStatus() extends Module {
    val tag_rs = io.CDB_io.tag
 
    // 32 register status, initilized to 0
-   val reg_stat = Vec.fill(32) { Reg(Uint(0), REG_BITS) }
+   val reg_stat = Vec.fill(32) { Reg(UInt(0)) }
 
    when (io.ena) {
 
-      tag_rs := reg_stat(issue_io.rs)
-      tag_rt := reg_stat(issue_io.rt)
+      issue_io.tag_rs := reg_stat(issue_io.rs)
+      issue_io.tag_rt := reg_stat(issue_io.rt)
 
       reg_stat(issue_io.rd) := issue_io.tag_rd
 
       for (i <- 0 until reg_stat.length) {
 
-         if (reg_stat(i) === tag_rs) {
+         when (reg_stat(i) === tag_rs) {
 
             reg_stat(i) := UInt(0)
             RF_io.rd_en := Bool(true)
-            RF_io.rd_addr := Bits(REG_BITS)
-
-            i = reg_stat.length
+            RF_io.rd_addr := Bits(i)
 
          }.otherwise {
             RF_io.rd_en := Bool(false)
-            RF_io.rd_addr := 0
+            RF_io.rd_addr := Bits(0)
          }
       }
    }
